89ab7a5d261a099daa155076dcdf910a8b495c65
superior-prog <meherajmahmmd@gmail.com>
2021-01-09 02:16:38 +0600

    Update settings.py

--- jobland/settings.py
+++ jobland/settings.py
@@ -139,7 +139,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Dhaka'
 
 USE_I18N = True
 
@@ -162,3 +162,11 @@ STATICFILES_DIRS = [
 MEDIA_URL = '/images/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
+
+# Email seetings
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = '587'
+EMAIL_HOST_USER = 'officialjobland777@gmail.com'
+EMAIL_HOST_PASSWORD = "asdfASDF123"
+EMAIL_USE_TLS = True  # for security
