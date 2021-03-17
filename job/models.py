3a075bf88face842293772b598c74402102a69eb
Meheraj <57069206+superior-prog@users.noreply.github.com>
2021-03-17 22:40:32 +0600

    Update models.py

--- job/models.py
+++ job/models.py
@@ -20,7 +20,7 @@ class JobModel(models.Model):
     image = models.ImageField(null=True, blank=True)
     job_title = models.CharField(max_length=255, null=True, blank=True)
     location = models.CharField(max_length=255, null=True, blank=True)
-    experience=models.IntegerField(null=True, blank=True)
+    experience = models.IntegerField(null=True, blank=True)
     job_type = models.CharField(max_length=30, choices=JOB_TYPE_CHOICES, null=True, blank=True)
     vacancy = models.CharField(max_length=255, null=True, blank=True)
     responsibilities = RichTextField(null=True, blank=True)
@@ -43,17 +43,17 @@ class JobModel(models.Model):
 class AppliedJobModel(models.Model):
     applicant = models.ForeignKey(User, null=True, blank=True, on_delete=models.CASCADE)
     job = models.ForeignKey(JobModel, null=True, blank=True, on_delete=models.CASCADE)
-    percentage=models.IntegerField(default=0,blank=True,null=True)
+    percentage = models.IntegerField(default=0, blank=True, null=True)
     timestamp = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.applicant.name + " applied to " + self.job.job_title
 
+
 class FilledJobModel(models.Model):
     company = models.CharField(max_length=1000, null=True, blank=True)
     job = models.CharField(max_length=1000, null=True, blank=True)
-    feedback=models.CharField(max_length=1000,null=True,blank=True)
+    feedback = models.CharField(max_length=1000, null=True, blank=True)
 
     def __str__(self):
         return self.company + " satisfied with " + self.job
-
